52f80b5beb98580753e1f6c56568268f57cd49c5
KoiyBottra <kbottra.student@puthisastra.edu.kh>
2020-07-29 17:05:45 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+ //function hideURLbar(){ window.scrollTo(0,1); } 
+ 
+ 
+ </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+</head>
+<body>
+<!--header-->
+<!--sticky-->
+<?php include('function.php'); ?>
+
+<?php include('top.php'); ?>
+<!--/sticky-->
+<?php include('slider.php'); ?>
+<!--About-->
+     <div class="about section" id="section-2">
+	  <div class="about-head text-center">
+	  <h3>About Us</h3>
+	  </div>
+	   <div class="container">		  
+		 <div class="col-md-4 about-grids">
+			 <h4>Our Vision</h4>
+			 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		 </div>
+		 <div class="col-md-4 about-grids grid2">
+			 <h4>Our Mission</h4>
+			 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		 </div>
+		 <div class="col-md-4 about-grids">
+			 <h4>Safety Information</h4>
+			 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		 </div>
+	 </div>
+</div>
+<!--/About-->
+<!--top-tours-->	
+<div  class="section" id="section-3">
+<div id="portfolio" class="portfolio">
+   <div class="top-tours-head text-center">
+		  <h3>Gallery</h3>		 
+		  </div>
+	      <ul id="filters" class="clearfix wow bounceIn" data-wow-delay="0.4s">
+			<li><span class="filter active" data-filter="app card icon logo fun">ALL</span></li>
+			<li><span class="filter" data-filter="app">Romantic Travel</span></li>
+			<li><span class="filter" data-filter="card">Foreign Travel</span></li>
+			<li><span class="filter" data-filter="icon">Short Date Tour</span></li>
+			<li><span class="filter" data-filter="fun">Long Date Tour</span></li>
+	        </ul>
+	     <div id="portfoliolist">
+					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t1.jpg" class="img-responsive" alt=""/></a>
+							</div>
+					</div>				
+					<div class="portfolio app mix_all" data-cat="app" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t2.jpg" class="img-responsive" alt=""/></a>
+                             </div>
+					</div>		
+					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t3.jpg" class="img-responsive" alt=""/></a>
+							
+					</div>
+					</div>				
+					<div class="portfolio icon mix_all" data-cat="icon" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t4.jpg" class="img-responsive" alt=""/></a>
+							 
+						</div>
+					</div>	
+					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t5.jpg" class="img-responsive" alt=""/></a>
+							 
+						</div>
+					</div>			
+					<div class="portfolio fun mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t6.jpg" class="img-responsive" alt=""/></a>
+							
+						</div>
+			      </div>
+				  <div class="portfolio fun mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+							<img src="images/t7.jpg" class="img-responsive" alt=""/></a>
+							
+						</div>
+			      </div>
+				  <div class="portfolio icon mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t8.jpg" class="img-responsive" alt=""/></a>
+							 
+					   </div>
+			      </div>
+		   <div class="clearfix"></div>	
+	  </div>
+</div>
+</div>  
+
+<div> 
+<?php include('bottom.php'); ?>
+<?php // Return to PHP.
+include('template.html'); // Include the footer.
+?>
+<?php // Return to PHP.
+include('templates/footer.html'); // Include the footer.
+?>
+</div>
+</body>
+</html>
+
---
+++ template.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="HandheldFriendly" content="True">
+    <title>Demo Template</title>
+    <link rel="stylesheet" type= "text/css" media="screen" href="css/concise.min.css" />
+    <link rel="stylesheet" type= "text/css" media="screen" href="css/masthead.css" />
+</head>
+<body>
+    <main container class= "siteContent">
+        <!-- BEGIN CHANGEABLE CONTENT. --><!-- END CHANGEABLE CONTENT. -->
+    </main>
+    <footer container class="siteFooter">
+        <p>Bottra uses <a href="http://concisecss.com/">| group membersk</a></p>
+    </footer>
+</body>
+</html>
